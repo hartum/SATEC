6d03a72ef932a134148e66a6b2a52a7004801fd3
hartum <hartum@gmail.com>
2023-03-05 08:43:14 +0100

    Added Helvetica found, and general style added to project

--- control-salas/src/main.js
+++ control-salas/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import axios from 'axios'
 import store from './store'
 import VueAxios from 'vue-axios'
+import '@/assets/general.css'
 import '/node_modules/primeflex/primeflex.min.css'
 
 createApp(App).use(VueAxios, axios).use(store).mount('#app')
