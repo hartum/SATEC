c570105afd1f93e6bf08273e5ae771e06c454722
hartum <hartum@gmail.com>
2023-03-05 08:38:19 +0100

    install axios for API request

--- control-salas/src/main.js
+++ control-salas/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 import store from './store'
+import VueAxios from 'vue-axios'
 import '/node_modules/primeflex/primeflex.min.css'
 
-createApp(App).use(store).mount('#app')
+createApp(App).use(VueAxios, axios).use(store).mount('#app')
