e12106ce1e293867081d5e9e3ae7a756d4ca83c5
hartum <hartum@gmail.com>
2023-03-05 08:35:05 +0100

    primeflex installed for responsive

--- control-salas/src/main.js
+++ control-salas/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import '/node_modules/primeflex/primeflex.min.css'
 
 createApp(App).use(store).mount('#app')
